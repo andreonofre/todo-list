1b4cf9784733b4f7b25b990ad04fdc01e04757aa
andreonofre <alordandre@gmail.com>
2023-09-02 01:17:51 -0300

    funcao adicionar todo

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import { useState } from 'react'
 
-const TodoForm = () => {
+const TodoForm = ({addTodo}) => {
     const [value, setValue] = useState("");
 
-    console.log(value)
 
     const handleSubmit = (event) => {
        event.preventDefault();
 
-       console.log(value)
+       //Passando o estado para o Wrapper
+       addTodo(value);// Passando o que foi digitado ao clicar no botão 
+      //console.log(value)
+      setValue("");
     };
     
   return (
     <form className='TodoForm' onSubmit={handleSubmit}>
-        <input type="text" className='todo-input' placeholder='Qual a sua tarefa para Hoje?' onChange={(e) => setValue(e.target.value)}/>
+        <input type="text" value={value} className='todo-input' placeholder='Qual a sua tarefa para Hoje?' onChange={(e) => setValue(e.target.value)}/>
         <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )
 }
 
-export default TodoForm
+export default TodoForm;
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import TodoForm from './TodoForm.jsx';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Todo from './Todo.jsx';
+
+uuidv4()// Incializando gerador de ID
+
 
 const TodoWrapper = () => {
+
+  const [todos, setTodos] = useState([]);
+
+  //Recebendo tarefa no parametro e add no obj
+  const addTodo = (todo) => {
+    setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}]); //Claonando estado atual e passando um obj novo
+    console.log(todos);
+  };
+
+
   return (
+  
     <div className='TodoWrapper' >
-        <TodoForm />
+        <TodoForm addTodo={addTodo}/>
+        <Todo />
     </div>
   )
 }
